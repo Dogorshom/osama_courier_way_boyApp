d53d83f0e7500a341b7c58c481796dda0bbb5b05
Dogorshom <saloom.dogorshom@gmail.com>
2021-05-21 09:27:34 +0200

    add dart convert

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -3,7 +3,7 @@ import 'package:courierway_deliveryboy/functions/localizations.dart';
 import 'package:courierway_deliveryboy/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'dart:convert';
 class Map extends StatefulWidget {
   @override
   _MapState createState() => _MapState();
